f2f3dbd8af8a60ca62e15b9419e820237dc37ba6
PASINDU-PRABHATH <prabhathdream@gmail.com>
2024-12-14 18:32:58 +0530

    Connect to Firestore Ubdate Discover Screen

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -26,7 +26,7 @@ class _MainPageState extends State<MainPage> {
   void initState() {
     
     super.initState();
-    FirebaseServices.getHotels();
+    FirebaseServices.getHotels(context);
   }
 
   @override
